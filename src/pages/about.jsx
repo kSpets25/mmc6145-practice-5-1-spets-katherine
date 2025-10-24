883613110c021a78e0758704e62c827e27507ee9
kSpets25 <142757766+kSpets25@users.noreply.github.com>
2025-10-23 21:50:02 -0500

    initialized files
    added inital files with index and about pages with link.

---
+++ src/pages/about.jsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+
+export default function About() {
+    return (
+      
+      <>
+      <h1>Hello from the about page!</h1>
+      <Link href="/">Go to Home Page</Link>
+      
+      </>
+    )
+  }
+  
---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    
+    <>
+    <h1>Hello from the home page!</h1>
+    <Link href="/about">Go to About Page</Link>
+    </>
+  )
+}
